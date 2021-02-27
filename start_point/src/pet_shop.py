c230eef62c50cdce24ea5f4fa03cff3d9a9d85f4
jaeltg <jaeltg@hotmail.com>
2021-02-27 17:50:03 +0000

    added add/remove cash, get pets sold and increase pets sold

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -4,3 +4,13 @@ def get_pet_shop_name(pet_shop):
    
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop, cash):
+    pet_shop["admin"]["total_cash"] += cash
+
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
+#Can I include the previous function within this function?
+def increase_pets_sold(pet_shop, pets):
+    pet_shop["admin"]["pets_sold"] += pets

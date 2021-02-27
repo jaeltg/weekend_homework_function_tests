1e596ace9fdfe1b6268effab28aafac98811e227
jaeltg <jaeltg@hotmail.com>
2021-02-27 18:51:19 +0000

    added functions for get stock count, get pets by breed, find pet by name, remove pet by name

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -14,3 +14,34 @@ def get_pets_sold(pet_shop):
 #Can I include the previous function within this function?
 def increase_pets_sold(pet_shop, pets):
     pet_shop["admin"]["pets_sold"] += pets
+
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
+
+def get_pets_by_breed(pet_shop, breed):
+    pets_filtered = []
+
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed:
+            pets_filtered.append(pet)
+
+    return pets_filtered        
+
+def find_pet_by_name(pet_shop, name):
+    result_pet = None
+
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            result_pet = pet
+
+    return result_pet        
+
+def remove_pet_by_name(pet_shop, name):
+
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            pet_shop["pets"].remove(pet)
+
+
+
+            
